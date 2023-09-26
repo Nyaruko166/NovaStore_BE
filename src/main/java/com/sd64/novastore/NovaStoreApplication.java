0d8204b367c54cf85d94729c7de04b482d39254e
Nyaruko166 <occho1666@gmail.com>
2023-09-26 22:58:39 +0700

    Init

---
+++ src/main/java/com/sd64/novastore/NovaStoreApplication.java
@@ -0,0 +1,13 @@
+package com.sd64.novastore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovaStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NovaStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sd64/novastore/NovaStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sd64.novastore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovaStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
