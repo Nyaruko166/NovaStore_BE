f27d9fd9224f5c2611228e61ab8cb05c51735fcf
Nyaruko166 <occho1666@gmail.com>
2023-10-12 13:30:21 +0700

    Update package

--- src/main/java/com/sd64/novastore/controller/cac.java
+++
@@ -1,4 +0,0 @@
-package com.sd64.novastore.controller;
-
-public class cac {
-}
---
+++ src/main/java/com/sd64/novastore/response/PaymentResponse.java
@@ -0,0 +1,20 @@
+package com.sd64.novastore.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+public class PaymentResponse implements Serializable {
+
+    private String vnp_ResponseCode;
+    private String vnp_CardType;
+    private String vnp_Amount;
+    private String vnp_TransactionNo;
+    private String vnp_PayDate;
+    private String vnp_OrderInfo;
+    private String vnp_BankTranNo;
+
+}
