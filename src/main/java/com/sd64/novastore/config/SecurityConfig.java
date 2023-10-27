64389b39609ce82b29bba6b4946bb1735810f1c1
Nyaruko166 <occho1666@gmail.com>
2023-10-27 20:55:14 +0700

    Update SecurityConfig

--- src/main/java/com/sd64/novastore/config/SecurityConfig.java
+++ src/main/java/com/sd64/novastore/config/SecurityConfig.java
@@ -5,10 +5,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.CsrfDsl;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -43,28 +45,26 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-                .authorizeHttpRequests(req ->
-                        {
-                            try {
-                                req.requestMatchers("/login", "/register", "/admin/account/page",
-                                                "/css/**", "/js/**", "/*.html", "/assets/**"
-                                        )
-                                        .permitAll()
-                                        .and()
-                                        .formLogin(form -> form
-                                                .loginPage("/login")
-                                                .defaultSuccessUrl("/admin/account/page")
-                                                .loginProcessingUrl("/login")
-                                                .permitAll())
-                                        .logout(logout -> logout
-                                                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                                                .permitAll());
-                            } catch (Exception e) {
-                                throw new RuntimeException(e);
-                            }
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(req -> {
+                            //resources exclude
+                            req.requestMatchers("/login", "/register", "/admin/account/page",
+                                            "/css/**", "/js/**", "/*.html", "/assets/**")
+                                    .permitAll();
+
+                            //Role
+                            req.anyRequest().authenticated();
                         }
                 );
+
+        http.formLogin(form -> form
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/admin/account/page")
+                        .loginProcessingUrl("/login")
+                        .permitAll())
+                .logout(logout -> logout
+                        .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                        .permitAll());
 
 //        http.authorizeHttpRequests().requestMatchers(new AntPathRequestMatcher("/resources/**"));
 
