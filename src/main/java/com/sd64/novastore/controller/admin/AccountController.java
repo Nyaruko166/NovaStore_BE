9051c89eb2370faa5d4309be79e58a6aaa721ce5
Nyaruko166 <occho1666@gmail.com>
2023-11-01 20:47:00 +0700

    update Account

--- src/main/java/com/sd64/novastore/controller/admin/AccountController.java
+++ src/main/java/com/sd64/novastore/controller/admin/AccountController.java
@@ -2,6 +2,7 @@ package com.sd64.novastore.controller.admin;
 
 import com.sd64.novastore.model.Account;
 import com.sd64.novastore.service.AccountService;
+import com.sd64.novastore.service.RoleService;
 import com.sd64.novastore.utils.SecurityUtil;
 import jakarta.validation.Valid;
 import org.codehaus.groovy.util.StringUtil;
@@ -14,12 +15,15 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 
-
 @Controller
 @RequestMapping("/nova/account")
 public class AccountController {
+
     @Autowired
     private AccountService accountService;
+
+    @Autowired
+    private RoleService roleService;
 
 //    @GetMapping("/all")
 //    public String getAll() {
@@ -30,7 +34,8 @@ public class AccountController {
     public String getAllPT(@RequestParam(defaultValue = "0", value = "page") Integer page, Model model) {
         Page<Account> pageAccount = accountService.getAllPT(page);
         model.addAttribute("pageAccount", pageAccount.getContent());
-        System.out.println(SecurityUtil.getSessionUser());
+        model.addAttribute("lstRole", roleService.getAll());
+//        System.out.println(SecurityUtil.getSessionUser());
         return "admin/account/account";
     }
 
@@ -38,6 +43,7 @@ public class AccountController {
     public String detail(@PathVariable Integer id, Model model) {
         Account account = accountService.findOne(id);
         model.addAttribute("account", account);
+        model.addAttribute("lstRole", roleService.getAll());
         return "/admin/account/account-detail";
     }
 
@@ -52,8 +58,9 @@ public class AccountController {
     @PostMapping("/add")
     public String add(@ModelAttribute("Account") Account account,
                       @RequestParam("avt") MultipartFile avt,
+                      @RequestParam("roleId") Integer roleId,
                       RedirectAttributes redirectAttributes) {
-        accountService.add(account, avt);
+        accountService.add(account, avt, roleId);
         redirectAttributes.addFlashAttribute("mess", "Thêm thành công!!");
         return "redirect:/nova/account/page";
     }
@@ -61,8 +68,10 @@ public class AccountController {
     @PostMapping("/update/{id}")
     public String update(@ModelAttribute("Account") Account account,
                          @RequestParam("avt") MultipartFile avt,
-                         @PathVariable Integer id, RedirectAttributes redirectAttributes) {
-        accountService.update(account, avt, id);
+                         @RequestParam("roleId") Integer roleId,
+                         @PathVariable Integer id,
+                         RedirectAttributes redirectAttributes) {
+        accountService.update(account, avt, roleId, id);
         redirectAttributes.addFlashAttribute("mess", "Update thành công!!");
         return "redirect:/nova/account/page";
     }
