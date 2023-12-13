012deab15949a30485ec142657034700f11549e7
Nyaruko166 <occho1666@gmail.com>
2023-12-13 18:54:13 +0700

    Delete From Cart

--- src/main/java/com/sd64/novastore/service/PaymentService.java
+++ src/main/java/com/sd64/novastore/service/PaymentService.java
@@ -1,6 +1,8 @@
 package com.sd64.novastore.service;
 
 import com.google.gson.JsonObject;
+import com.sd64.novastore.model.Address;
+import com.sd64.novastore.model.Cart;
 import jakarta.servlet.http.HttpServletRequest;
 
 import java.io.IOException;
@@ -9,9 +11,9 @@ import java.net.URISyntaxException;
 
 public interface PaymentService {
 
-    JsonObject vnpayCreate(HttpServletRequest req, Long price, String specificAddress, String ward, String district, String city, String name, String phoneNumber, String email, String voucher) throws UnsupportedEncodingException;
+    JsonObject vnpayCreate(HttpServletRequest req, Long price, String address) throws UnsupportedEncodingException;
 
-    JsonObject zalopayCreate(Long amount, String specificAddress, String ward, String district, String city, String name, String phoneNumber, String email, String voucher) throws IOException, URISyntaxException;
+    JsonObject zalopayCreate(Long amount, String address) throws IOException, URISyntaxException;
 
-    JsonObject MomoPayCreate(Long amount, String specificAddress, String ward, String district, String city, String name, String phoneNumber, String email, String voucher) throws IOException, URISyntaxException;
+    JsonObject MomoPayCreate(Long amount, String address) throws IOException, URISyntaxException;
 }
