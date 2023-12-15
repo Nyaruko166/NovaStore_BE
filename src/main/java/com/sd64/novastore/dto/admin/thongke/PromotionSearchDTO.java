258a52d0fd74adfd91d617b7edf7c69d145d1275
LuongXuanHung123 <hungluongxuan413@gmail.com>
2023-12-16 00:15:03 +0700

    cập nhật hhihi

---
+++ src/main/java/com/sd64/novastore/dto/admin/thongke/PromotionSearchDTO.java
@@ -0,0 +1,13 @@
+package com.sd64.novastore.dto.admin.thongke;
+
+public interface PromotionSearchDTO {
+    Integer getId();
+
+    String getCode();
+
+    String getName();
+
+    Float getValue();
+
+    Integer getStatus();
+}
