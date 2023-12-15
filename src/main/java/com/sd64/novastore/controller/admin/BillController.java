2bb4b0351b748090cff6157903f8197fe9a51dde
mihtu4n <chuminhtuan.tt23@gmail.com>
2023-12-15 13:27:17 +0700

    update bill

--- src/main/java/com/sd64/novastore/controller/admin/BillController.java
+++ src/main/java/com/sd64/novastore/controller/admin/BillController.java
@@ -1,16 +1,28 @@
 package com.sd64.novastore.controller.admin;
 
+import com.sd64.novastore.dto.admin.BillDto;
 import com.sd64.novastore.model.Bill;
 import com.sd64.novastore.model.BillDetail;
+import com.sd64.novastore.model.PaymentMethod;
 import com.sd64.novastore.service.BillService;
+import com.sd64.novastore.service.PaymentMethodService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.math.BigDecimal;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 import java.util.List;
 
 @Controller
@@ -20,22 +32,77 @@ public class BillController {
     @Autowired
     private BillService billService;
 
-    @GetMapping("/page")
-    public String getAllPT(@RequestParam(defaultValue = "0", value = "page") Integer page,
-                           Model model) {
-        Page<Bill> pageBill = billService.getAllBillPT(page);
-        model.addAttribute("pageBill", pageBill.getContent());
-        model.addAttribute("totalElements", pageBill.getTotalElements());
-        return "/admin/bill/bill";
+    @Autowired
+    private PaymentMethodService paymentMethodService;
+
+    @GetMapping()
+    public String getAllBills(Model model,
+                              @RequestParam(defaultValue = "0", value = "page") Integer page,
+                              @RequestParam(name = "sort", defaultValue = "createDate,desc") String sortField,
+                              @RequestParam(name = "code", required = false) String code,
+                              @RequestParam(name = "ngayTaoStart", required = false) @DateTimeFormat(pattern = "yyyy-MM-dd") Date ngayTaoStart,
+                              @RequestParam(name  = "ngayTaoEnd", required = false) @DateTimeFormat(pattern = "yyyy-MM-dd") Date ngayTaoEnd,
+                              @RequestParam(name = "status", required = false) Integer status,
+                              @RequestParam(name = "type", required = false) Integer type,
+                              @RequestParam(name = "phoneNumber", required = false) String phoneNumber,
+                              @RequestParam(name = "customerName", required = false) String customerName) {
+        int pageSize = 8;
+        String[] sortParams = sortField.split(",");
+        String sortFieldName = sortParams[0];
+        Sort.Direction sortDirection = Sort.Direction.ASC;
+
+        if (sortParams.length > 1 && sortParams[1].equalsIgnoreCase("desc")) {
+            sortDirection = Sort.Direction.DESC;
+        }
+
+        Sort sort = Sort.by(sortDirection, sortFieldName);
+        Pageable pageable = PageRequest.of(page, pageSize, sort);
+
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+        Page<BillDto> listBill;
+        if (ngayTaoStart != null || ngayTaoEnd != null || code != null || status != null || type != null || customerName != null || phoneNumber != null) {
+            if(ngayTaoStart != null) {
+                model.addAttribute("ngayTaoStart", formatter.format(ngayTaoStart));
+            }
+            if(ngayTaoEnd != null) {
+                model.addAttribute("ngayTaoEnd", formatter.format(ngayTaoEnd));
+            }
+            listBill = billService.searchListBill(code.trim(), ngayTaoStart, ngayTaoEnd, status, type, phoneNumber.trim(), customerName.trim(), pageable);
+        } else {
+            listBill = billService.findAll(pageable);
+        }
+
+        model.addAttribute("sortField", sortFieldName);
+        model.addAttribute("sortDirection", sortDirection);
+        model.addAttribute("items", listBill);
+
+        model.addAttribute("code", code);
+        model.addAttribute("status", status);
+        model.addAttribute("type", type);
+        model.addAttribute("phoneNumber", phoneNumber);
+        model.addAttribute("customerName", customerName);
+        model.addAttribute("sortField", sortField);
+        return "admin/bill/bill";
     }
 
     @GetMapping("/detail/{id}")
-    public String detail(@PathVariable Integer id,
-                         Model model) {
+    public String getBillDetail(Model model, @PathVariable("id") Integer id) {
+        Bill bill = billService.getOneBill(id);
         List<BillDetail> lstBillDetails = billService.getLstDetailByBillId(id);
-        model.addAttribute("page", lstBillDetails);
-        return "/admin/bill/bill-detail";
+        List<PaymentMethod> listPaymentMethod = paymentMethodService.getAllBillPaymentMethod(id);
+        model.addAttribute("bill", bill);
+        model.addAttribute("lstBillDetails", lstBillDetails);
+        model.addAttribute("listPaymentMethod", listPaymentMethod);
+        return "admin/bill/bill-detail";
     }
+
+//    @GetMapping("/detail/{id}")
+//    public String detail(@PathVariable Integer id,
+//                         Model model) {
+//        List<BillDetail> lstBillDetails = billService.getLstDetailByBillId(id);
+//        model.addAttribute("page", lstBillDetails);
+//        return "/admin/bill/bill-detail";
+//    }
 
     @RequestMapping(value = "/cancel-bill/{id}", method = {RequestMethod.PUT, RequestMethod.GET})
     public String cancelBill(@PathVariable Integer id, RedirectAttributes attributes) {
--- src/main/java/com/sd64/novastore/repository/BillRepository.java
+++ src/main/java/com/sd64/novastore/repository/BillRepository.java
@@ -1,5 +1,6 @@
 package com.sd64.novastore.repository;
 
+import com.sd64.novastore.dto.admin.BillDto;
 import com.sd64.novastore.model.Bill;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -8,6 +9,8 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.List;
 
 @Repository
@@ -23,4 +26,30 @@ public interface BillRepository extends JpaRepository<Bill, Integer> {
 
     @Query("SELECT b FROM Bill b WHERE b.customer.id = :customerId ORDER BY b.orderDate DESC")
     List<Bill> getAllOrders(@Param("customerId") Integer customerId);
+
+    @Query(value = "SELECT b.id AS id, b.code AS code, b.customerName AS customerName, b.phoneNumber " +
+            "            AS phoneNumber, b.orderDate AS createDate, b.totalPrice AS totalPrice, b.type " +
+            "            AS type, b.status AS status" +
+            "            FROM Bill b")
+    Page<BillDto> listBill(Pageable pageable);
+
+    @Query(value = "SELECT b.id AS id,b.code AS code, b.customerName AS customerName, b.phoneNumber " +
+            "            AS phoneNumber,b.orderDate AS createDate, b.totalPrice AS totalPrice, b.type " +
+            "            AS type, b.status AS status" +
+            "            FROM Bill b " +
+            "WHERE (:code IS NULL OR b.code LIKE CONCAT('%', :code, '%')) " +
+            "AND (:ngayTaoStart IS NULL OR :ngayTaoEnd IS NULL OR (b.orderDate BETWEEN :ngayTaoStart AND :ngayTaoEnd)) " +
+            "AND (:status IS NULL OR b.status = :status) " +
+            "AND (:type IS NULL OR b.type= :type) "+
+            "AND (:phoneNumber IS NULL OR b.phoneNumber IS NULL OR b.phoneNumber LIKE CONCAT('%', :phoneNumber, '%')) "+
+            "AND (:customerName IS NULL OR b.customerName IS NULL OR b.customerName LIKE CONCAT('%', :customerName, '%'))")
+    Page<BillDto> listSearchBill(
+            @Param("code") String code,
+            @Param("ngayTaoStart") Date ngayTaoStart,
+            @Param("ngayTaoEnd") Date ngayTaoEnd,
+            @Param("status") Integer status,
+            @Param("type") Integer type,
+            @Param("phoneNumber") String phoneNumber,
+            @Param("customerName") String customerName,
+            Pageable pageable);
 }
