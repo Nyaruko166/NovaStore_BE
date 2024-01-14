419568b857680d87e8c60e99b607cd6b137fd57d
LuongXuanHung123 <hungluongxuan413@gmail.com>
2024-01-14 22:03:38 +0700

    done1

--- src/main/java/com/sd64/novastore/service/impl/PromotionServiceImpl.java
+++ src/main/java/com/sd64/novastore/service/impl/PromotionServiceImpl.java
@@ -2,7 +2,6 @@ package com.sd64.novastore.service.impl;
 
 import com.sd64.novastore.dto.admin.PromotionDetailDTO;
 import com.sd64.novastore.dto.admin.thongke.PromotionSearchDTO;
-import com.sd64.novastore.model.Material;
 import com.sd64.novastore.model.Product;
 import com.sd64.novastore.model.ProductDetail;
 import com.sd64.novastore.model.Promotion;
@@ -108,13 +107,13 @@ public class PromotionServiceImpl implements PromotionService {
 
     @Override
     public Boolean add(Promotion promotion) {
-        if (checkName(promotion.getName())){
+        if (checkName(promotion.getName())) {
             promotion.setCreateDate(new Date());
             promotion.setUpdateDate(new Date());
             promotion.setCode(generateCode());
             promotion.updateStatus();
-             promotionRepository.save(promotion);
-             return true;
+            promotionRepository.save(promotion);
+            return true;
         }
         return false;
     }
@@ -134,39 +133,37 @@ public class PromotionServiceImpl implements PromotionService {
 
     @Override
     public Boolean update(Promotion promotion, Integer id) {
-       if (checkName(promotion.getName())){
-           Optional<Promotion> optional = promotionRepository.findById(id);
-           if (optional.isPresent()) {
-               Promotion oldPromotion = optional.get();
-               promotion.setId(oldPromotion.getId());
-               promotion.setCode(oldPromotion.getCode());
-               promotion.setCreateDate(oldPromotion.getCreateDate());
-               promotion.setUpdateDate(new Date());
-               promotion.setStatus(oldPromotion.getStatus());
-               promotion.updateStatus();
-               if (oldPromotion.getStatus() != promotion.getStatus() && promotion.getStatus() == 2) {
-                   for (PromotionDetail promotionDetail : oldPromotion.getPromotionDetails()) {
-                       Product product = promotionDetail.getProduct();
-                       if (product.getStatus() == 2) {
-                           product.setStatus(1);
-                           productRepository.save(product);
-                           List<ProductDetail> productDetails = giamGiaRepository.findByProductId(product.getId());
-                           for (ProductDetail productDetail : productDetails) {
-                               productDetail.setPriceDiscount(productDetail.getPrice());
-                               productDetailRepository.save(productDetail);
-                               List<PromotionDetail> promotionDetails = promotionDetailRepository.findByPromotionId(id);
-                               promotionDetailRepository.deleteAll(promotionDetails);
-                           }
-                       }
-                   }
-               }
-                promotionRepository.save(promotion);
-               return true;
-           } else {
-               return false;
-           }
-       }
-       return false;
+
+        Optional<Promotion> optional = promotionRepository.findById(id);
+        if (optional.isPresent()) {
+            Promotion oldPromotion = optional.get();
+            promotion.setId(oldPromotion.getId());
+            promotion.setCode(oldPromotion.getCode());
+            promotion.setCreateDate(oldPromotion.getCreateDate());
+            promotion.setUpdateDate(new Date());
+            promotion.setStatus(oldPromotion.getStatus());
+            promotion.updateStatus();
+            if (oldPromotion.getStatus() != promotion.getStatus() && promotion.getStatus() == 2) {
+                for (PromotionDetail promotionDetail : oldPromotion.getPromotionDetails()) {
+                    Product product = promotionDetail.getProduct();
+                    if (product.getStatus() == 2) {
+                        product.setStatus(1);
+                        productRepository.save(product);
+                        List<ProductDetail> productDetails = giamGiaRepository.findByProductId(product.getId());
+                        for (ProductDetail productDetail : productDetails) {
+                            productDetail.setPriceDiscount(productDetail.getPrice());
+                            productDetailRepository.save(productDetail);
+                            List<PromotionDetail> promotionDetails = promotionDetailRepository.findByPromotionId(id);
+                            promotionDetailRepository.deleteAll(promotionDetails);
+                        }
+                    }
+                }
+            }
+            promotionRepository.save(promotion);
+            return true;
+        } else {
+            return false;
+        }
     }
 
 
